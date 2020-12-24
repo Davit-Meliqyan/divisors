ac0a9575f77d142963a1c599d4d5dd20076d465e
Davit-Meliqyan <76562656+Davit-Meliqyan@users.noreply.github.com>
2020-12-24 22:14:55 +0400

    Add files via upload

---
+++ divisors.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class divisors {
+    public static void main(String [] args){
+
+        Scanner scaner = new Scanner(System.in);
+
+        System.out.println("insert the number");
+
+        int j=scaner.nextInt();
+
+        System.out.println("divisors of number");
+        for( int i=1 ; i<=j ; i++){
+            if( j % i != 0 ) continue;
+            System.out.println(i);
+        }
+
+    }
+}
